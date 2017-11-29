f7055f4851f691940f8b83d011dac1b4cc03853c
Nicolas Escallier-duront <nicolas.escallier@free.fr>
2017-11-29 11:39:59 +0100

    TP2 ajout exemple 2

---
+++ tp2/ObjetEntier.java
@@ -0,0 +1,15 @@
+package fr.unice.miage.tp2;
+
+public class ObjetEntier {
+    private int ma_valeur;
+    public ObjetEntier()    {
+	ma_valeur=0; 
+	System.out.format("Valeur partagee initialisee a %d\n", ma_valeur); 
+    }
+    public String  toString(){
+	return Integer.toString (ma_valeur);    }
+    public int val(){ return ma_valeur;}
+    public     void add(int i) { ma_valeur+=i;} 
+
+} 
+   
